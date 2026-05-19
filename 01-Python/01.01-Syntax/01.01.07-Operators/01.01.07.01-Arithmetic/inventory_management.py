fce8d171f76419967f822c5a2f39a8503c1a153a
Oli <oli.dare.dev@gmail.com>
2026-05-19 19:37:47 +0200

    fix: stock-logic

--- 01-Python/01.01-Syntax/01.01.07-Operators/01.01.07.01-Arithmetic/inventory_management.py
+++ 01-Python/01.01-Syntax/01.01.07-Operators/01.01.07.01-Arithmetic/inventory_management.py
@@ -8,7 +8,8 @@ box_capacity = 10
 base_price = 12.50
 price_increase_factor = 0.05
 
-total_stock = new_delivery_boxes * items_per_box * stock
-remaining_stock = stock - customer_order
-individual_items = remaining_stock % items_per_box
+total_stock = stock + new_delivery_boxes * items_per_box
+remaining_stock = total_stock - customer_order
+individual_items = remaining_stock % box_capacity
+full_boxes = remaining_stock // box_capacity
 new_unit_price =  base_price + base_price * 0.05
