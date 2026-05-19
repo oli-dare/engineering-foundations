6b97b3da047ca0204be1f017dbae875949f01619
Oli <oli.dare.dev@gmail.com>
2026-05-19 19:15:28 +0200

    feat: budget-calc

---
+++ 01-Python/01.01-Syntax/01.01.07-Operators/01.01.07.01-Arithmetic/arithmetic_ops.py
@@ -0,0 +1,11 @@
+# Calculate monthly savings
+
+budget = 2500
+fixed_costs = 1245
+variable_costs = 380
+repair_cost = 215
+
+savings = budget - fixed_costs - variable_costs - repair_cost
+daily_sparring = savings / 30
+print(daily_sparring)
+
