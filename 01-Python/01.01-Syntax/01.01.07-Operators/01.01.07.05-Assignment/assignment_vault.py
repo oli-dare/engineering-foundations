c11af25a1ee8fb53852145ae4d895f30a4d5435d
Oli <oli.dare.dev@gmail.com>
2026-05-20 21:31:35 +0200

    feat: assignment-operators

---
+++ 01-Python/01.01-Syntax/01.01.07-Operators/01.01.07.05-Assignment/assignment_vault.py
@@ -0,0 +1,17 @@
+# assignment operator mastery
+
+score = 10
+
+# My implementation here
+score += 5
+print(score)
+score *= 2
+print(score)
+score /= 3
+print(score)
+score //= 2
+print(score)
+score %= 2
+print(score)
+score **= 2
+print(score)
